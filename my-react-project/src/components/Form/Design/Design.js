1af43f849b69d1b155f7000fa2ac1a50657655fa
Yanira <yanirafr@gmail.com>
2020-11-08 12:38:56 +0100

    add dropdown function to design

--- my-react-project/src/components/Form/Design/Design.js
+++ my-react-project/src/components/Form/Design/Design.js
@@ -1,66 +1,79 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from 'react';
-import FormMenu from '../FormMenu/FormMenu';
-import './Design.scss';
+import React, { Component } from "react";
+import FormMenu from "../FormMenu/FormMenu";
+import "./Design.scss";
+import propTypes from "prop-types";
 
 class Design extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+    this.hide = "shown";
+    this.handleDropdown = this.handleDropdown.bind(this);
+  }
+  handleDropdown(ev) {
+    if (this.hide === "shown") {
+      this.hide = "hidden";
+    } else {
+      this.hide = "shown";
+    }
 
-	render() {
-		return (
-			<fieldset className="design">
-				<FormMenu
-					fieldsetName="design"
-					fieldsetTitle="Diseña"
-					fieldsetIcon="far fa-object-ungroup"
-				/>
-				<div className="js-design colors">
-					<h3 className="colors__title">Colores</h3>
-					<div className="color__palets">
-						<div className="color-palette-1">
-							<input
-								className="js-palette-green js-palettes input1"
-								type="radio"
-								id="color-palette-1"
-								name="color"
-								value="color-palette-1"
-								checked
-							/>
-							<div className="palette1-1"></div>
-							<div className="palette1-2"></div>
-							<div className="palette1-3"></div>
-						</div>
-						<div className="color-palette-2">
-							<input
-								className="js-palette-red js-palettes input2"
-								type="radio"
-								id="color-palette-2"
-								name="color"
-								value="color-palette-2"
-							/>
-							<div className="palette2-1"></div>
-							<div className="palette2-2"></div>
-							<div className="palette2-3"></div>
-						</div>
-						<div className="color-palette-3">
-							<input
-								className="js-palette-grey js-palettes input3"
-								type="radio"
-								id="color-palette-3"
-								name="color"
-								value="color-palette-3"
-							/>
-							<div className="palette3-1"></div>
-							<div className="palette3-2"></div>
-							<div className="palette3-3"></div>
-						</div>
-					</div>
-				</div>
-			</fieldset>
-		);
-	}
+    this.forceUpdate();
+  }
+
+  render() {
+    return (
+      <fieldset className="design">
+        <FormMenu
+          fieldsetName="design"
+          fieldsetTitle="Diseña"
+          fieldsetIcon="far fa-object-ungroup"
+          handleMenuClick={this.handleDropdown}
+        />
+        <div className={"js-design colors " + this.hide}>
+          <h3 className="colors__title">Colores</h3>
+          <div className="color__palets">
+            <div className="color-palette-1">
+              <input
+                className="js-palette-green js-palettes input1"
+                type="radio"
+                id="color-palette-1"
+                name="color"
+                value="color-palette-1"
+                checked
+              />
+              <div className="palette1-1"></div>
+              <div className="palette1-2"></div>
+              <div className="palette1-3"></div>
+            </div>
+            <div className="color-palette-2">
+              <input
+                className="js-palette-red js-palettes input2"
+                type="radio"
+                id="color-palette-2"
+                name="color"
+                value="color-palette-2"
+              />
+              <div className="palette2-1"></div>
+              <div className="palette2-2"></div>
+              <div className="palette2-3"></div>
+            </div>
+            <div className="color-palette-3">
+              <input
+                className="js-palette-grey js-palettes input3"
+                type="radio"
+                id="color-palette-3"
+                name="color"
+                value="color-palette-3"
+              />
+              <div className="palette3-1"></div>
+              <div className="palette3-2"></div>
+              <div className="palette3-3"></div>
+            </div>
+          </div>
+        </div>
+      </fieldset>
+    );
+  }
 }
 
 export default Design;
--- my-react-project/src/components/Form/FormMenu/FormMenu.js
+++ my-react-project/src/components/Form/FormMenu/FormMenu.js
@@ -1,29 +1,41 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from 'react';
-import menuIcon from '../../../images/fridacolor.png';
-import './FormMenu.scss';
+import React, { Component } from "react";
+import menuIcon from "../../../images/fridacolor.png";
+import "./FormMenu.scss";
+import propTypes from "prop-types";
 
 class FormMenu extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(ev) {
+    this.props.handleMenuClick(ev);
+  }
 
-	render() {
-		return (
-			<div className={"js-" + this.props.fieldsetName + "-collapsable"}>
-				<legend className="legend">
-					<i className={this.props.fieldsetIcon + " legend__icon"}></i>
-					<h2 className="legend__title">{this.props.fieldsetTitle}</h2>
-					<img
-						src={menuIcon}
-						alt="Menu"
-						className={"js-arrow-" + this.props.fieldsetName + "legend__arrow arrow-up"}
-						aria-label="Desplegar"
-					/>
-				</legend>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div
+        className={"js-" + this.props.fieldsetName + "-collapsable"}
+        onClick={this.handleClick}
+      >
+        <legend className="legend">
+          <i className={this.props.fieldsetIcon + " legend__icon"}></i>
+          <h2 className="legend__title">{this.props.fieldsetTitle}</h2>
+          <img
+            src={menuIcon}
+            alt="Menu"
+            className={
+              "js-arrow-" + this.props.fieldsetName + "legend__arrow arrow-up"
+            }
+            aria-label="Desplegar"
+          />
+        </legend>
+      </div>
+    );
+  }
 }
+
+FormMenu.propTypes = {};
 
 export default FormMenu;
