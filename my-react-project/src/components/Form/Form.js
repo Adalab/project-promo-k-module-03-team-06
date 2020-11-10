650c805515c7a76183486da183088a03f261a1cc
GemaPujalte <gema.pujalte.iglesias@gmail.com>
2020-11-10 14:45:40 +0100

    add event and state to FormMenu

--- my-react-project/src/components/Form/Form.js
+++ my-react-project/src/components/Form/Form.js
@@ -3,6 +3,7 @@ import "./Form.scss";
 import Design from "./Design/Design";
 import Fill from "./Fill/Fill";
 import Share from "./Share/Share";
+import FormMenu from "./FormMenu/FormMenu";
 
 class Form extends Component {
   constructor(props) {
@@ -18,9 +19,37 @@ class Form extends Component {
           method="POST"
           role="form"
         >
-          <Design />
-          <Fill />
-          <Share />
+          <fieldset className="design">
+            <FormMenu
+              fieldsetName="design"
+              fieldsetTitle="Diseña"
+              fieldsetIcon="far fa-object-ungroup"
+            >
+              <Design />
+            </FormMenu>
+          </fieldset>
+
+          <fieldset>
+            <div className="fill">
+              <FormMenu
+                fieldsetName="fill"
+                fieldsetTitle="Rellena"
+                fieldsetIcon="far fa-keyboard"
+              >
+                <Fill />
+              </FormMenu>
+            </div>
+          </fieldset>
+
+          <fieldset className="form-share">
+            <FormMenu
+              fieldsetName="share"
+              fieldsetTitle="Comparte"
+              fieldsetIcon="fas fa-share-alt"
+            >
+              <Share />
+            </FormMenu>
+          </fieldset>
         </form>
       </section>
     );
--- my-react-project/src/components/Form/FormMenu/FormMenu.js
+++ my-react-project/src/components/Form/FormMenu/FormMenu.js
@@ -1,29 +1,55 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from 'react';
-import menuIcon from '../../../images/fridacolor.png';
-import './FormMenu.scss';
+import React, { Component } from "react";
+import menuIcon from "../../../images/fridacolor.png";
+import "./FormMenu.scss";
 
 class FormMenu extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
 
-	render() {
-		return (
-			<div className={"js-" + this.props.fieldsetName + "-collapsable"}>
-				<legend className="legend">
-					<i className={this.props.fieldsetIcon + " legend__icon"}></i>
-					<h2 className="legend__title">{this.props.fieldsetTitle}</h2>
-					<img
-						src={menuIcon}
-						alt="Menu"
-						className={"js-arrow-" + this.props.fieldsetName + "legend__arrow arrow-up"}
-						aria-label="Desplegar"
-					/>
-				</legend>
-			</div>
-		);
-	}
+    this.state = {
+      collapse: "hidden",
+    };
+
+    this.handleCollapse = this.handleCollapse.bind(this);
+  }
+
+  handleCollapse() {
+    this.setState(() => {
+      let isCollapse;
+      if (this.state.collapse === "hidden") {
+        isCollapse = "";
+      } else {
+        isCollapse = "hidden";
+      }
+      return { collapse: isCollapse };
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div
+          className={"js-" + this.props.fieldsetName + "-collapsable"}
+          onClick={this.handleCollapse}
+        >
+          <legend className="legend">
+            <i className={this.props.fieldsetIcon + " legend__icon"}></i>
+            <h2 className="legend__title">{this.props.fieldsetTitle}</h2>
+            <img
+              src={menuIcon}
+              alt="Menu"
+              className={
+                "js-arrow-" + this.props.fieldsetName + "legend__arrow arrow-up"
+              }
+              aria-label="Desplegar"
+            />
+          </legend>
+        </div>
+        <div className={this.state.collapse}>{this.props.children}</div>
+      </>
+    );
+  }
 }
 
 export default FormMenu;
