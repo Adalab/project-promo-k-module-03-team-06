76563d06edb519d62e1c9138cd454baa5dad131f
alicia-colom <aliciacolom@gmail.com>
2020-11-11 15:14:25 +0100

    Fix icons in menus

--- my-react-project/src/components/Form/Form.js
+++ my-react-project/src/components/Form/Form.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 import React, { Component } from "react";
 import "./Form.scss";
 import Design from "./Design/Design";
--- my-react-project/src/components/Form/FormMenu/FormMenu.js
+++ my-react-project/src/components/Form/FormMenu/FormMenu.js
@@ -1,55 +1,59 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from "react";
-import menuIcon from "../../../images/fridacolor.png";
-import "./FormMenu.scss";
+import React, { Component } from 'react';
+import menuIcon from '../../../images/fridacolor.png';
+import './FormMenu.scss';
 
 class FormMenu extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      collapse: "hidden",
-    };
+		this.state = {
+			collapse: 'hidden',
+			// arrowUp: '',
+		};
 
-    this.handleCollapse = this.handleCollapse.bind(this);
-  }
+		this.handleCollapse = this.handleCollapse.bind(this);
+	}
 
-  handleCollapse() {
-    this.setState(() => {
-      let isCollapse;
-      if (this.state.collapse === "hidden") {
-        isCollapse = "";
-      } else {
-        isCollapse = "hidden";
-      }
-      return { collapse: isCollapse };
-    });
-  }
+	handleCollapse() {
+		this.setState(() => {
+			let isCollapse;
+			// let arrowUp;
+			if (this.state.collapse === 'hidden') {
+				isCollapse = '';
+				// arrowUp = 'arrow-up';
+			} else {
+				isCollapse = 'hidden';
+				// arrowUp = '';
+			}
+			return { collapse: isCollapse };
+		});
+	}
 
-  render() {
-    return (
-      <>
-        <div
-          className={"js-" + this.props.fieldsetName + "-collapsable"}
-          onClick={this.handleCollapse}
-        >
-          <legend className="legend">
-            <i className={this.props.fieldsetIcon + " legend__icon"}></i>
-            <h2 className="legend__title">{this.props.fieldsetTitle}</h2>
-            <img
-              src={menuIcon}
-              alt="Menu"
-              className={
-                "js-arrow-" + this.props.fieldsetName + "legend__arrow arrow-up"
-              }
-              aria-label="Desplegar"
-            />
-          </legend>
-        </div>
-        <div className={this.state.collapse}>{this.props.children}</div>
-      </>
-    );
-  }
+	render() {
+		return (
+			<div className={this.state.collapse}>
+				<div
+					className={'js-' + this.props.fieldsetName + '-collapsable'}
+					onClick={this.handleCollapse}
+				>
+					<legend className="legend">
+						<i className={this.props.fieldsetIcon + ' legend__icon'}></i>
+						<h2 className="legend__title">{this.props.fieldsetTitle}</h2>
+						<img
+							src={menuIcon}
+							alt="Menu"
+							className={
+								'js-arrow-' + this.props.fieldsetName + ' legend__arrow'
+							}
+							aria-label="Desplegar"
+						/>
+					</legend>
+				</div>
+				<div className="hide">{this.props.children}</div>
+			</div>
+		);
+	}
 }
 
 export default FormMenu;
