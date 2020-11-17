e84d228c4744d6eb14d901a7200186484004d148
mteresa-jimenez <mteresa.jn@gmail.com>
2020-11-17 13:28:24 +0100

    Add event image (no completed)

--- my-react-project/src/components/Form/Fill/InputImage/InputImage.js
+++ my-react-project/src/components/Form/Fill/InputImage/InputImage.js
@@ -1,37 +1,35 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from 'react';
-import './InputImage.scss';
 
-class InputImage extends Component {
-	constructor(props) {
-		super(props);
-	}
+import "./InputImage.scss";
 
-	render() {
-		return (
-			<div className="fill-image">
-				<div className="form">
-					<label htmlFor="img-selector">Imagen de perfil</label>
-					<button
-						className="js__profile-trigger fill-image__btn"
-						type="button"
-					>
-						Añadir imagen
-					</button>
-					<input
-						type="file"
-						name="img-selector"
-						id="img-selector"
-						className="js__profile-upload-btn fill-image__hidden"
-					/>
-				</div>
-				<div className="form fill-image__profile">
-					<div className="js__profile-image"></div>
-					<div className="js__profile-preview profile__preview"></div>
-				</div>
-			</div>
-		);
-	}
-}
+const InputImage = (props) => {
+  const getImage = () => {
+    const image = getImage.current.files[0];
+    console.log(image);
+  };
+
+  return (
+    <div className="fill-image">
+      <div className="form">
+        <label htmlFor="img-selector">Imagen de perfil</label>
+        <button className="js__profile-trigger fill-image__btn" type="button">
+          Añadir imagen
+        </button>
+        <input
+          type="file"
+          name="img-selector"
+          id="img-selector"
+          className="js__profile-upload-btn fill-image__hidden"
+          accept="image/*"
+          onChange={getImage}
+        />
+      </div>
+      <div className="form fill-image__profile">
+        <div className="js__profile-image"></div>
+        <div className="js__profile-preview profile__preview"></div>
+      </div>
+    </div>
+  );
+};
 
 export default InputImage;
