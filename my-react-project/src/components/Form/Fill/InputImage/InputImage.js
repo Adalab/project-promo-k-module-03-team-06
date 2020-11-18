4d3ddd076a422034f787190dc897573136f18198
mteresa-jimenez <mteresa.jn@gmail.com>
2020-11-18 14:49:30 +0100

    Add false click function in InputImage component

--- my-react-project/src/components/Form/Fill/InputImage/InputImage.js
+++ my-react-project/src/components/Form/Fill/InputImage/InputImage.js
@@ -1,21 +1,31 @@
 /* eslint-disable no-useless-constructor */
-
+import React from "react";
 import "./InputImage.scss";
+import ReactDOM from "react-dom";
 
 const InputImage = (props) => {
   const getImage = () => {
     const image = getImage.current.files[0];
     console.log(image);
   };
+  const realClick = React.createRef();
+  const handleFalseClick = () => {
+    realClick.current.click();
+  };
 
   return (
     <div className="fill-image">
       <div className="form">
         <label htmlFor="img-selector">Imagen de perfil</label>
-        <button className="js__profile-trigger fill-image__btn" type="button">
+        <button
+          onClick={handleFalseClick}
+          className="js__profile-trigger fill-image__btn"
+          type="button"
+        >
           Añadir imagen
         </button>
         <input
+          ref={realClick}
           type="file"
           name="img-selector"
           id="img-selector"
